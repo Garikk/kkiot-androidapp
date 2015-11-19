f9deee3c28dcd6e1e46ff59e4f7ff82dd6f9ad48
Garikk <supergarikk@gmail.com>
2015-11-19 20:33:52 +0300

    Prepare for Google market

--- app/src/androidTest/java/kkdev/kksystem/kkdev/kkcarandroid/ApplicationTest.java
+++ app/src/androidTest/java/kkdev/kksystem/kkdev/kkcarandroid/ApplicationTest.java
@@ -1,4 +1,4 @@
-package android.kksystem.kkdev.kkcarandroid;
+package kkdev.kksystem.kkdev.kkcarandroid;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/android/kksystem/kkdev/kkcarandroid/android/kksystem/kkdev/kkcarandroid/manager/MediaOperations.java
+++
@@ -1,7 +0,0 @@
-package android.kksystem.kkdev.kkcarandroid.android.kksystem.kkdev.kkcarandroid.manager;
-
-/**
- * Created by blinov_is on 18.11.2015.
- */
-public class MediaOperations {
-}
