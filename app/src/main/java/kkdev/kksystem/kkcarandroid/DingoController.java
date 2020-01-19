d5004a2965774fca89963b89f47c42947ac484db
Garikk <supergarikk@gmail.com>
2020-01-20 00:13:28 +0300

    android host continue

--- app/src/main/java/kkdev/kksystem/kkcarandroid/DingoController.java
+++ app/src/main/java/kkdev/kksystem/kkcarandroid/DingoController.java
@@ -4,14 +4,18 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import java.io.IOException;
+
 import kkdev.kksystem.kkcarandroid.constants.SysConstants;
 import kkdev.kksystem.kkcontroller.main.KKController;
 
 public class DingoController extends Service {
-    KKController controller;
     public DingoController() {
-        controller = new KKController();
-        controller.InitSystem(SysConstants.SystemProfiles());
+        try {
+            KKController.InitSystem(SysConstants.SystemProfiles());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
